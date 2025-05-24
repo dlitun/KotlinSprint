3b9b5cf9ea67cb540132f4cb39912278841c326a
dlitun <dlitun@ya.ru>
2025-05-25 00:26:18 +0400

    KS-1-1: change variables

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -3,9 +3,5 @@ package lesson_1
 fun main() {
     val totalOrdersCount: Int = 75
     val thankYouMessage: String = "Спасибо за покупку!"
-    val ordersText: String = "$totalOrdersCount заказов"
-
-    println(ordersText)
-    println(thankYouMessage)
 }
 
