c428630bab5af9877c0437acb9eada5f0248ef05
dlitun <dlitun@ya.ru>
2025-05-29 15:35:17 +0400

    KS-2-1: add arithmetic

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -1,2 +1,12 @@
 package org.example.lesson2
 
+fun main() {
+    val score1 = 3
+    val score2 = 4
+    val score3 = 3
+    val score4 = 5
+
+    val average = (score1 + score2 + score3 + score4) / 4.0
+
+    println(String.format("%.2f", average))
+}
