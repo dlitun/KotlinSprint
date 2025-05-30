3de1b1755d7046746c710161180ae6c4817523a6
dlitun <dlitun@ya.ru>
2025-05-30 18:51:42 +0400

    KS-2-5: change arithmetic

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -2,9 +2,10 @@ package org.example.lesson2
 
 fun main() {
     val initialDeposit = 70000.0
-    val interestRate = 0.167
+    val interestRatePercent = 16.7
     val years = 20
 
+    val interestRate = interestRatePercent / 100
     val finalAmount = initialDeposit * Math.pow(1 + interestRate, years.toDouble())
 
     println(String.format("%.3f", finalAmount))
