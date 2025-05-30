2681452d9968e7a775a9405729e073f781ba8302
dlitun <dlitun@ya.ru>
2025-05-30 18:45:59 +0400

    KS-2-3: change arithmetic

--- src/main/kotlin/lesson2/lesson3_task1.kt
+++ src/main/kotlin/lesson2/lesson3_task1.kt
@@ -5,11 +5,14 @@ fun main() {
     val departureMinute = 39
     val travelTimeMinutes = 457
 
-    val totalDepartureMinutes = departureHour * 60 + departureMinute
+    val minutesInHour = 60
+    val hoursInDay = 24
+
+    val totalDepartureMinutes = departureHour * minutesInHour + departureMinute
     val arrivalTotalMinutes = totalDepartureMinutes + travelTimeMinutes
 
-    val arrivalHour = (arrivalTotalMinutes / 60) % 24
-    val arrivalMinute = arrivalTotalMinutes % 60
+    val arrivalHour = (arrivalTotalMinutes / minutesInHour) % hoursInDay
+    val arrivalMinute = arrivalTotalMinutes % minutesInHour
 
     println("Поезд прибудет в $arrivalHour:${arrivalMinute.toString().padStart(2, '0')}")
 }
