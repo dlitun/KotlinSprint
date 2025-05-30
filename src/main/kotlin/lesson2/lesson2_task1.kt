80f23ebaacc9bd770552e9d97b81b3a9187c8369
dlitun <dlitun@ya.ru>
2025-05-30 18:34:01 +0400

    KS-2-1: change arithmetic

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -5,8 +5,9 @@ fun main() {
     val score2 = 4
     val score3 = 3
     val score4 = 5
+    val studentCount = 4
 
-    val average = (score1 + score2 + score3 + score4) / 4.0
+    val average = (score1 + score2 + score3 + score4) / studentCount.toDouble()
 
     println(String.format("%.2f", average))
 }
