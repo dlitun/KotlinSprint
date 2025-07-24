9f4ccfdc9874f671f04bce1a81be7f83d16f1b21
dlitun <dlitun@ya.ru>
2025-07-24 17:48:05 +0400

    KS-3-5: create string

---
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -0,0 +1,14 @@
+package lesson3
+
+fun main() {
+    val moveString = "D2-D4;0"
+
+    val from = "D2"
+    val to = "D4"
+    val moveNumber = "0"
+
+    println("$from-$to;$moveNumber")
+    println("Откуда: $from")
+    println("Куда: $to")
+    println("Номер хода: $moveNumber")
+}
