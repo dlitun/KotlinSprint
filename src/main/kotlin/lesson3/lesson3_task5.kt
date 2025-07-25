c0f0c39f952992a50ee5f9f4e0da4b399b86985d
dlitun <dlitun@ya.ru>
2025-07-25 14:03:57 +0400

    KS-3-5: change string

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -3,9 +3,7 @@ package lesson3
 fun main() {
     val moveString = "D2-D4;0"
 
-    val from = "D2"
-    val to = "D4"
-    val moveNumber = "0"
+    val (from, to, moveNumber) = moveString.split("-", ";")
 
     println("$from-$to;$moveNumber")
     println("Откуда: $from")
