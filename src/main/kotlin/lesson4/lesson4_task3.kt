e9445d1e28637e29a8a10ed8b443c9216c31a6f5
dlitun <dlitun@ya.ru>
2025-07-30 17:49:39 +0400

    KS-4-3: change boolean

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -1,12 +1,15 @@
 package lesson4_task3
 
+const val REQUIRED_HUMIDITY: Int = 20
+const val REQUIRED_SEASON: String = "зима"
+const val IS_SUNNY: Boolean = true
+const val IS_TENT_OPEN: Boolean = true
+
 fun main() {
-    val isSunny: Boolean = true
-    val isTentOpen: Boolean = true
+
     val humidity: Int = 20
     val season: String = "зима"
-
-    val areConditionsFavorable = isSunny && isTentOpen && humidity == 20 && season != "зима"
+    val areConditionsFavorable = IS_SUNNY && IS_TENT_OPEN && humidity == REQUIRED_HUMIDITY && season != REQUIRED_SEASON
 
     println("Благоприятные ли условия сейчас для роста бобовых? $areConditionsFavorable")
 }
