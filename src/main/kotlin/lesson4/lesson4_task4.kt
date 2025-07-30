f34bdb7f5f3c3aa6ef7b28b78580790e80d695d3
dlitun <dlitun@ya.ru>
2025-07-30 17:54:50 +0400

    KS-4-4: change boolean

--- src/main/kotlin/lesson4/lesson4_task4.kt
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -2,18 +2,12 @@ package lesson4_task4
 
 fun main() {
     val day = 5
-
     val isEvenDay = day % 2 == 0
 
-    val arms = !isEvenDay
-    val abs = !isEvenDay
-    val legs = isEvenDay
-    val back = isEvenDay
-
     println("""
-        Упражнения для рук:    $arms
-        Упражнения для ног:    $legs
-        Упражнения для спины:  $back
-        Упражнения для пресса: $abs
+        Упражнения для рук:    ${!isEvenDay}
+        Упражнения для ног:    $isEvenDay
+        Упражнения для спины:  $isEvenDay
+        Упражнения для пресса: ${!isEvenDay}
     """.trimIndent())
 }
