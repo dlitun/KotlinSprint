ec80af54cbc081666a1519b1acfe867793152079
dlitun <dlitun@ya.ru>
2025-07-31 11:35:37 +0400

    KS-4-5: change boolean

--- src/main/kotlin/lesson4/lesson4_task5.kt
+++ src/main/kotlin/lesson4/lesson4_task5.kt
@@ -1,10 +1,13 @@
 package lesson4_task5
 
-fun main() {
-    val MIN_CREW = 55
-    val MAX_CREW = 70
-    val MIN_FOOD_BOXES = 50
+const val MIN_CREW = 55
+const val MAX_CREW = 70
+const val MIN_FOOD_BOXES = 50
 
+const val HAS_NO_DAMAGE = false
+const val IS_WEATHER_GOOD = true
+
+fun main() {
     println("Есть ли повреждения корпуса (true/false):")
     val hasDamage = readln().toBoolean()
 
@@ -17,8 +20,8 @@ fun main() {
     println("Погода благоприятная? (true/false):")
     val isGoodWeather = readln().toBoolean()
 
-    val canSail = (!hasDamage && crew in MIN_CREW..MAX_CREW && foodBoxes > MIN_FOOD_BOXES) ||
-            (hasDamage && crew == MAX_CREW && isGoodWeather && foodBoxes >= MIN_FOOD_BOXES)
+    val canSail = (hasDamage == HAS_NO_DAMAGE && crew in MIN_CREW..MAX_CREW && foodBoxes > MIN_FOOD_BOXES) ||
+            (hasDamage != HAS_NO_DAMAGE && crew == MAX_CREW && isGoodWeather == IS_WEATHER_GOOD && foodBoxes >= MIN_FOOD_BOXES)
 
     println("Корабль может отправиться в плавание: $canSail")
 }
