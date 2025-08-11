4432ba7b81240a13c4633435add7260255900750
dlitun <dlitun@ya.ru>
2025-08-11 18:42:51 +0400

    KS-6-4: create while-do-while

---
+++ src/main/kotlin/lesson6/lesson6_task4.kt
@@ -0,0 +1,32 @@
+package lesson6_task4
+
+const val MIN = 1
+const val MAX = 9
+const val MAX_ATTEMPTS = 5
+
+fun main() {
+    val secretNumber = (MIN..MAX).random()
+    var attempts = MAX_ATTEMPTS
+
+    while (attempts > 0) {
+        print("Введите число от $MIN до $MAX: ")
+        val guess = readln().toIntOrNull()
+
+        if (guess == null || guess !in MIN..MAX) {
+            println("Введите корректное число от $MIN до $MAX!")
+            continue
+        }
+
+        if (guess == secretNumber) {
+            println("Это была великолепная игра!")
+            return
+        } else {
+            attempts--
+            if (attempts > 0) {
+                println("Неверно. Осталось попыток: $attempts")
+            } else {
+                println("Было загадано число $secretNumber")
+            }
+        }
+    }
+}
