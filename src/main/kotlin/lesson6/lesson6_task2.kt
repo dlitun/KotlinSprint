0a7fcd8e4aa1561afb88405507912613bbb8e9a1
dlitun <dlitun@ya.ru>
2025-08-11 17:49:22 +0400

    KS-6-2: change while-do-while

--- src/main/kotlin/lesson6/lesson6_task2.kt
+++ src/main/kotlin/lesson6/lesson6_task2.kt
@@ -4,13 +4,9 @@ const val ONE_SECOND_MS = 1000L
 
 fun main() {
     print("Введите количество секунд: ")
-    val seconds = readLine()!!.toInt()
-    var counter = seconds
+    val seconds = readln().toInt()
 
-    while (counter > 0) {
-        Thread.sleep(ONE_SECOND_MS)
-        counter--
-    }
+    Thread.sleep(seconds * ONE_SECOND_MS)
 
     println("Прошло $seconds секунд")
 }
