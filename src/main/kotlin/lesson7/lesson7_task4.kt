ecc0895a9bc2a73dcfab701ef510b15ee35c989d
dlitun <dlitun@ya.ru>
2025-08-31 22:50:31 +0400

    KS-7-4: create for-range

---
+++ src/main/kotlin/lesson7/lesson7_task4.kt
@@ -0,0 +1,13 @@
+package lesson7_task4
+
+fun main() {
+    print("Введите количество секунд: ")
+    val seconds = readLine()!!.toInt()
+
+    for (i in seconds downTo 1) {
+        println("Осталось $i секунд(ы)")
+        Thread.sleep(1000)
+    }
+
+    println("Время вышло")
+}
