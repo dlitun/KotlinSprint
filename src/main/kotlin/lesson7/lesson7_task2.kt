3df3d34f1453c0517425539fdc1c9c5f27a2a5f6
dlitun <dlitun@ya.ru>
2025-09-01 23:16:22 +0400

    KS-7-2: change for-range

--- src/main/kotlin/lesson7/lesson7_task2.kt
+++ src/main/kotlin/lesson7/lesson7_task2.kt
@@ -2,19 +2,21 @@ package lesson7_task2
 
 fun main() {
     val codes: IntRange = 1000..9999
+    var code: Int
 
-    while (true) {
-        val code = codes.random()
-
+    do {
+        code = codes.random()
         println("Ваш код авторизации: $code")
-        print("Введите код: ")
-        val input = readLine()
 
-        if (input == code.toString()) {
+        print("Введите код: ")
+        val input = readln().toInt()
+
+        if (input == code) {
             println("Добро пожаловать!")
             break
         } else {
             println("Неверный код. Отправляем новый...\n")
         }
-    }
+
+    } while (true)
 }
