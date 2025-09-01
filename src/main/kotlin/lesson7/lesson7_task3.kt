20658ba6b384ae2d48e9dc9c2e837481a0d9528f
dlitun <dlitun@ya.ru>
2025-09-01 23:24:12 +0400

    KS-7-3: change for-range

--- src/main/kotlin/lesson7/lesson7_task3.kt
+++ src/main/kotlin/lesson7/lesson7_task3.kt
@@ -2,7 +2,7 @@ package lesson7_task3
 
 fun main() {
     print("Введите число: ")
-    val n = readLine()!!.toInt()
+    val n = readln().toInt()
 
     for (i in 0..n step 2) {
         println(i)
