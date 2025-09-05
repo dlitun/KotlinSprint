e8b6e3dcad86544487fad1e9f0b1ab1c578f50a2
dlitun <dlitun@ya.ru>
2025-09-05 17:28:35 +0400

    KS-9-1: change lists

--- src/main/kotlin/lesson8/lesson8_task1.kt
+++ src/main/kotlin/lesson8/lesson8_task1.kt
@@ -2,9 +2,6 @@ package lesson8_task1
 
 fun main() {
     val viewsPerDay = arrayOf(42, 77, 122, 111, 45, 156, 131)
-    var totalViews = 0
-    for (views in viewsPerDay) {
-        totalViews += views
-    }
+    val totalViews = viewsPerDay.sum()
     println("Всего просмотров за неделю: $totalViews")
 }
