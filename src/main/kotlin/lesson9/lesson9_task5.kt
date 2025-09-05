ddb965138cf68586209c8a14236d442f2043cf48
dlitun <dlitun@ya.ru>
2025-09-05 15:05:49 +0400

    KS-9-5: create lists

---
+++ src/main/kotlin/lesson9/lesson9_task5.kt
@@ -0,0 +1,19 @@
+package lesson9_task5
+
+fun main() {
+    val ingredients = mutableListOf<String>()
+
+    for (i in 1..5) {
+        print("Введите ингредиент №$i: ")
+        val input = readln()
+        ingredients.add(input.lowercase())
+    }
+
+    val uniqueSorted = ingredients.toSet().sorted()
+
+    val result = uniqueSorted.mapIndexed { index, item ->
+        if (index == 0) item.replaceFirstChar { it.uppercase() } else item
+    }
+
+    println(result.joinToString(", "))
+}
