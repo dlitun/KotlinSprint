a7583edddc7f3b50f3ccab3bbcbb6957948b584c
dlitun <dlitun@ya.ru>
2025-09-25 19:07:50 +0300

    KS-11-1: create classes-properties

---
+++ src/main/kotlin/lesson11/Main.kt
@@ -0,0 +1,20 @@
+package lesson_11
+
+fun main() {
+    val user1: User = User(
+        id = 1,
+        login = "dima123",
+        password = "qwerty123",
+        email = "dima@example.com",
+    )
+
+    val user2: User = User(
+        id = 2,
+        login = "anna2025",
+        password = "pass2025",
+        email = "anna@example.com",
+    )
+
+    println("User 1: id=${user1.id}, login=${user1.login}, password=${user1.password}, email=${user1.email}")
+    println("User 2: id=${user2.id}, login=${user2.login}, password=${user2.password}, email=${user2.email}")
+}
---
+++ src/main/kotlin/lesson11/User.kt
@@ -0,0 +1,8 @@
+package lesson_11
+
+class User(
+    val id: Int,
+    var login: String,
+    var password: String,
+    var email: String,
+)
