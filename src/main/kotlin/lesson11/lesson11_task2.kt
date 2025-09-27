b74d47864d14dff00637bd8fe688b8933ff57c4c
dlitun <dlitun@ya.ru>
2025-09-28 01:03:11 +0300

    KS-11-2: create classes-properties

---
+++ src/main/kotlin/lesson11/lesson11_task2.kt
@@ -0,0 +1,54 @@
+package lesson11_task2
+
+class User2(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val email: String,
+    var bio: String = ""
+) {
+
+    fun printInfo() {
+        println("User info:")
+        println("id = $id")
+        println("login = $login")
+        println("email = $email")
+        println("bio = $bio")
+        println()
+    }
+
+    fun setBio() {
+        println("Enter your bio:")
+        val input = readln()
+        bio = input
+        println("Bio has been updated!\n")
+    }
+
+    fun changePassword() {
+        println("Enter your current password:")
+        val current = readln()
+
+        if (current == password) {
+            println("Password is correct. Enter a new password:")
+            val newPass = readln()
+            password = newPass
+            println("Password has been successfully changed!\n")
+        } else {
+            println("Incorrect current password. Password was not changed.\n")
+        }
+    }
+}
+
+fun main() {
+    val user = User2(
+        id = 3,
+        login = "maria2025",
+        password = "12345",
+        email = "maria@example.com",
+    )
+
+    user.printInfo()
+    user.setBio()
+    user.changePassword()
+    user.printInfo()
+}
