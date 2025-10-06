3e09a870576fd46bf5346506e3fd7a52a06eed4e
dlitun <dlitun@ya.ru>
2025-10-06 18:56:32 +0300

    KS-12-1: create constructors-init

---
+++ src/main/kotlin/lesson12/lesson12_task1.kt
@@ -0,0 +1,33 @@
+package lesson12_task1
+
+class WeatherDay() {
+
+    var dayTemperature = 0
+    var nightTemperature = 0
+    var hasPrecipitation = false
+
+    fun printWeather() {
+        println("Day temperature: $dayTemperature deg C")
+        println("Night temperature: $nightTemperature deg C")
+        println("Precipitation: $hasPrecipitation")
+    }
+}
+
+fun main() {
+
+    val monday = WeatherDay()
+    monday.dayTemperature = 22
+    monday.nightTemperature = 14
+    monday.hasPrecipitation = true
+
+    val tuesday = WeatherDay()
+    tuesday.dayTemperature = 25
+    tuesday.nightTemperature = 16
+    tuesday.hasPrecipitation = false
+
+    println("Weather on Monday:")
+    monday.printWeather()
+
+    println("\nWeather on Tuesday:")
+    tuesday.printWeather()
+}
