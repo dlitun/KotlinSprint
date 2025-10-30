0e7881b4787b7962514759f278839994ed9a1b16
dlitun <dlitun@ya.ru>
2025-10-30 23:53:11 +0300

    KS-19-3: enum-companion-object

---
+++ src/main/kotlin/lesson19/lesson19_task3.kt
@@ -0,0 +1,19 @@
+package lesson19_task3
+
+class Spaceship {
+    fun launch() {
+    }
+
+    fun land() {
+        throw NotImplementedError("Landing method is not implemented yet")
+    }
+
+    fun shootAsteroid() {
+        println("The spaceship shoots an asteroid!")
+    }
+}
+
+fun main() {
+    val ship = Spaceship()
+    ship.shootAsteroid()
+}
