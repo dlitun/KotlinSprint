faa0701833f4015766e28c92b5aa914ca359644a
dlitun <dlitun@ya.ru>
2025-11-12 18:04:01 +0300

    KS-22-1: change data-class

--- src/main/kotlin/lesson22/lesson22_task1.kt
+++ src/main/kotlin/lesson22/lesson22_task1.kt
@@ -18,5 +18,8 @@ fun main() {
     val dataBook2 = DataBook("The Hitchhiker's Guide to the Galaxy", "Douglas Adams")
 
     println(regularBook1 == regularBook2)
+    // false - хотя книги выглядят одинаково, это два разных объекта в памяти
+
     println(dataBook1 == dataBook2)
+    // true - у дата-классов сравниваются значения свойств, а не место в памяти
 }
